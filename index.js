eba88e5778f2f2eb46c5bf6608a932f50f3d2466
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-04-29 14:00:35 +0900

    21.04.29(목) #1 THEORY
    확실히 이론공부는 지루하다.
    그래도 카카오톡 클론코딩 할때 보다 더 깔끔하고 체계적으로 정리하며 배우고 있다.

---
+++ index.js
@@ -0,0 +1,20 @@
+const nicoInfo = {
+    name: "Coder_Chu",
+    age: 24,
+    gender: "Male",
+    Handsome: true,
+    favMovie: ["Along the gods",
+    "Oldboy"],
+    favFood: [
+        {
+          name:"Kimchi",
+          fatty: false
+        },
+        {
+          name: "Cheese burger", 
+          fatty: true 
+        }
+    ]
+}
+
+console.log(nicoInfo);
