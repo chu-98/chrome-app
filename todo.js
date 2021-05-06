9edef2634b2dc9abf66cc3eec859f1255f5199fd
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-05-06 10:24:40 +0900

    21.05.06(목) #3 ToDo(3)
    설입에서 저녁약속이 있다.
    먼저 코딩 공부하고 운동하고
    카페가야겠다.

--- todo.js
+++ todo.js
@@ -4,7 +4,21 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        function filterFn(toDo) {
+            return toDo.id !== li.id;
+        }
+    });
+    toDos = cleanToDos
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,8 +28,9 @@ function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
-    const newId =  toDos.length + 1;
+    const newId = toDos.length + 1;
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
